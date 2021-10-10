4bd3acecb5a2b1e45596a861211348a0ae37cc99
Yauhen_Shydlouski <yauhen_shydlouski@epam.com>
2021-10-11 01:59:28 +0300

    I have finished subtask 1 bla-bla

---
+++ src/main/java/project/NewClassWithTask.java
@@ -0,0 +1,14 @@
+package project;
+
+public class NewClassWithTask {
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
