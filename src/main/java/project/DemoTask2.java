2d75380584d4fba1e743d8f0586bcc3757d95967
Yauhen_Shydlouski <yauhen_shydlouski@epam.com>
2021-10-24 22:39:06 +0300

    I completed DemoTask2

---
+++ src/main/java/project/DemoTask2.java
@@ -0,0 +1,31 @@
+package project;
+
+public class DemoTask2 {
+
+    private int intField;
+    private String stringField;
+
+    public DemoTask2(int intField, String stringField) {
+        this.intField = intField;
+        this.stringField = stringField;
+    }
+
+    public int getIntField() {
+        return intField;
+
+
+    }
+
+    public void setIntField(int intField) {
+        this.intField = intField;
+    }
+
+    public String getStringField() {
+        return stringField;
+    }
+
+    public void setStringField(String stringField) {
+        this.stringField = stringField;
+    }
+
+}
